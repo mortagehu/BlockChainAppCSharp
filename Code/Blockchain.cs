acc60a802f6e5a2face1c217ec78d0915b79bcf5
Daniel <63243766+mortagehu@users.noreply.github.com>
2024-08-28 20:04:12 +0200

    Add project files.

---
+++ Code/Blockchain.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainApp.Code
+{
+    internal class Blockchain
+    {
+        internal List<Block> Chain { get; set; } = new List<Block>();
+        internal int Difficulty { get; set; }
+        public Blockchain(int difficulty)
+        {
+            Difficulty = difficulty;
+            Chain = new List<Block>();
+            AddGenesisBlock();
+        }
+        internal void AddGenesisBlock()
+        {
+            Chain.Add(new Block(0,DateTime.Now,"GENESIS:BLOCK",""));
+        }
+
+        internal Block GetLatestBlock()
+        {
+            return Chain[Chain.Count - 1];
+        }
+        internal void AddBlock (Block block)
+        {
+            block.PreviousHash = GetLatestBlock().Hash;
+            block.MineBlock(Difficulty);
+            Chain.Add(block);
+        }
+        internal bool isChainValid()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block currentBlock = Chain[i];
+                Block previousBlock = Chain[i - 1];
+
+                if (currentBlock.Hash != currentBlock.CalculateHash())
+                {
+                    return false;
+                }
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using BlockChainApp.Code;
+using Newtonsoft.Json;
+
+
+Console.WriteLine("Blockchain starting...");
+
+List<Transaction> transactions = new List<Transaction>
+        {
+            new Transaction
+            {
+                Id = Guid.NewGuid(),
+                AccountId = Guid.NewGuid(),
+                TransactionDate = DateTime.Now,
+                TransactionAmount = 330500m,
+                Memo = "101 N Main St Greenwood, IN"
+            },
+            new Transaction
+            {
+                Id = Guid.NewGuid(),
+                AccountId = Guid.NewGuid(),
+                TransactionDate = DateTime.Now,
+                TransactionAmount = 474900m,
+                Memo = "421 Pearl St Columbus, IN"
+            },
+            new Transaction
+            {
+                Id = Guid.NewGuid(),
+                AccountId = Guid.NewGuid(),
+                TransactionDate = DateTime.Now,
+                TransactionAmount = 515000m,
+                Memo = "416 Banta St Franklin, IN"
+            }
+        };
+Blockchain blockchain = new Blockchain(2); 
+
+foreach (Transaction transaction in transactions)
+{
+    string data = JsonConvert.SerializeObject(transaction);
+
+    Console.WriteLine($"Adding block:{data}" );
+
+    Block block = new Block(blockchain.GetLatestBlock().Index + 1, DateTime.Now, JsonConvert.SerializeObject(transaction), blockchain.GetLatestBlock().Hash);
+    blockchain.AddBlock(block);
+
+}
+
+Console.WriteLine($"Checking if blockchain is valid: {blockchain.isChainValid()}");
+Console.WriteLine($"Blockchain: {JsonConvert.SerializeObject(blockchain, Formatting.Indented)}");
